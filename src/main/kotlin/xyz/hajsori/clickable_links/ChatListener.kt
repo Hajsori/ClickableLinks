c9b7679c4a618e018135f31a4e938a600f60c697
Hajsori <hajsori@proton.me>
2026-04-09 13:00:55 +0200

    Fixed sharing Xaero Waypoints and added underline decoration to clickable URLs

--- src/main/kotlin/xyz/hajsori/clickable_links/ChatListener.kt
+++ src/main/kotlin/xyz/hajsori/clickable_links/ChatListener.kt
@@ -3,22 +3,32 @@ package xyz.hajsori.clickable_links
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 
 class ChatListener : Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     fun onChat(event: AsyncChatEvent) {
         val message = PlainTextComponentSerializer.plainText().serialize(event.message())
         var newMessage = Component.empty()
 
+        if (!message.contains("https://") && !message.contains("http://")) {
+            return
+        }
+
         message.split(" ").forEach { word ->
-            if (word.startsWith("https://") || word.startsWith("http://")) {
-                newMessage = newMessage.append(Component.text(word).clickEvent(ClickEvent.clickEvent(ClickEvent.Action.OPEN_URL,
-                    ClickEvent.Payload.string(word)))).appendSpace()
+            newMessage = if (word.startsWith("https://") || word.startsWith("http://")) {
+                newMessage
+                    .append(Component.text(word)
+                        .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.OPEN_URL, ClickEvent.Payload.string(word)))
+                        .decorate(TextDecoration.UNDERLINED)
+                    )
+                    .appendSpace()
             } else {
-                newMessage = newMessage.append(Component.text(word)).appendSpace()
+                newMessage.append(Component.text(word)).appendSpace()
             }
         }
 
